1cf9d3ac275e9d9fad01bb7e4555fa81f5782628
Lorenzo Tunesi <lorenzo.tunesi@polito.it>
2025-06-05 16:13:33 +0200

    Added Grating Couplers - in and out

--- MZI.py
+++ MZI.py
@@ -52,10 +52,47 @@ class MZI_sw:
         contact.dmove(origin=(0,0),destination=(self.mmi_l+2*self.bend_r+self.mmi_taper_l+self.arm_l+self.wg_w-self.heater_w+self.pos[0],self.mmi_gap+2*self.bend_r+self.pos[1]))
         self.component.add_port(name=f"e_{self.instance}_2",port=contact["e2"])
 
+
+def add_grating_coupler(component, pos=[[0,140],[700,140]], fiberarray_spacing=100, fiberarray_clearance=200, 
+    layer_wg=(1,0), wg_w=0.5):
+    gdspath = os.path.join(os.getcwd(), "ANT_GC.GDS")
+    antgc = gf.read.import_gds(gdspath)
+
+    my_route_s = gf.cross_section.strip(
+        width=wg_w,                # same as route_width=5
+        layer=layer_wg           # same as your original routing_layer usage
+    )
+
+    antgc.add_port(
+        "o1",
+        center=(antgc.x, antgc.y - 19.95),
+        orientation=270,
+        width=wg_w,
+        layer=layer_wg,
+        )
+
+    grating_number = 3
+    for idx in range(2):
+        for i in range(grating_number):
+            antgc_ref = component << antgc.copy()
+            
+            antgc_ref.dmove(   # con dmove puoi spostare nel punto desiderato al posto che move "relativo"
+            origin=(antgc_ref.x, antgc_ref.y), # .x e .y ritornano il centro del componente
+            destination=(pos[idx][0]+((2*idx-1)*fiberarray_clearance),pos[idx][1]-(70*i)))
+            antgc_ref.drotate(angle=90+(idx*180), center=antgc_ref.center)
+
+            shadow_rect = component << gf.components.rectangle(size=(0.5, 0.5), layer=layer_wg, port_type="optical") # needed because add port is broken as of 9.7.0
+            shadow_rect.connect("o3", antgc_ref["o1"]),
+            component.add_port(f"Grating{idx}_{i}", port=shadow_rect["o1"])
+
+
+    return master_component
+
 master_component=gf.Component("Neuromorphic_Chip")
 MZI = MZI_sw(
     component=master_component
 )
+
 
 x_spacing = 0
 y_spacing = 70
@@ -73,7 +110,7 @@ MZI.create_structure(pos=(x_spacing,0))
 MZI.create_structure(pos=(x_spacing,y_spacing))
 MZI.create_structure(pos=(x_spacing,2*y_spacing))
 
-
+master_component=add_grating_coupler(master_component)
 
 
 master_component.pprint_ports()
